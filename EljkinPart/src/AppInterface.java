dfbd302806ead6208db24bc684c8c2fcc57ad967
maximelkin <maxelkin@list.ru>
2016-07-12 13:41:19 +0400

    Update AppInterface.java

--- EljkinPart/src/AppInterface.java
+++ EljkinPart/src/AppInterface.java
@@ -4,6 +4,7 @@ interface AppInterface {
     public void showBoard(int[][] board);
 
     //for getting name of player, and chosen figure instead of pawn
+    public String getString(String note);
 
     //to say, whose turn and notify about check, mate, stalemate
     public void notification(String note);
